dfa5857f9bc1e78bbdfbd1df38b2e3c4d3d4395c
AndyVorzkla <andreyshadrin007@gmail.com>
2022-02-03 17:28:00 +0300

    hide secret_key + fix cards length

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+#from env import secret_keys as secret
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(su8$(*@bpf6yu$y2(j15r!+!_y)' \
-             '5*4utz#4@fut=8c099kht!'
+#SECRET_KEY = os.getenv('SECRET_KEY', secret.SECRET_KEY)
+SECRET_KEY = "django-insecure-(su8$(*@bpf6yu$y2(j15r!+!_y)5*4utz#4@fut=8c099kht!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
